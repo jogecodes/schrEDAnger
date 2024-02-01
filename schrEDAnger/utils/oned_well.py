434e2b8cc5b72edd8f3f468a912f9739b2423d6e
jogecodes <jorge.casajus@alumnos.upm.es>
2024-02-01 10:42:22 +0100

    refactoring

--- schrEDAnger/utils/oned_well.py
+++ schrEDAnger/utils/oned_well.py
@@ -3,6 +3,18 @@ from numpy.linalg import norm
 from typing import Union
 
 
+# Obtiene la energía del n-ésimo estado del pozo infinito 1D
+def get_nth_state_energy(n_state: int, well_length=5):
+    if not isinstance(n_state, int):
+        raise ValueError("States must be quantized as integers")
+    ground_energy = (2 * np.pi) ** 2 / (8 * well_length**2)
+    if n_state > 0:
+        nth_state_energy = (n_state + 1) ** 2 * ground_energy
+        return nth_state_energy
+    else:
+        return ground_energy
+
+
 # Función de optimización basada en referencia: Direct solution of Schrödinger equation by GA
 def compute_z(
     x: Union[np.array, list], energy_lowbound=0, well_length=5, force_boundary=True
